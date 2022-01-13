6cddf5561a618cb4ba5d80e4096a5c837325c171
jeffDevelops <jefferydreynolds@gmail.com>
2022-01-13 13:30:12 -0700

    layout fixes

--- client/src/routes/checklist.svelte
+++ client/src/routes/checklist.svelte
@@ -10,14 +10,18 @@
 <Controls />
 
 <main class="checklist">
-  <h1>Checklist</h1>
-  <p>These are things you can do today to limit your participation in extractionist capitalism.</p>
+  <section>
+    <h1>Checklist</h1>
+    <p>
+      These are things you can do today to limit your participation in extractionist capitalism.
+    </p>
 
-  <ChecklistItem
-    participationLink={`/campaigns/1`}
-    checked={typeof window !== 'undefined' &&
-      localStorage.getItem(LocalStorageKeys.HasCompletedBankChecklistItem) === 'true'}
-  />
+    <ChecklistItem
+      participationLink={`/campaigns/1`}
+      checked={typeof window !== 'undefined' &&
+        localStorage.getItem(LocalStorageKeys.HasCompletedBankChecklistItem) === 'true'}
+    />
+  </section>
 
   <div class="disclosure">
     <Card>
@@ -27,20 +31,20 @@
         something like this?), and development of new features takes time.
       </p>
     </Card>
+  </div>
 
-    <div class="disclosure">
-      <Card>
-        <h4><IdeaIcon /> Have an idea for the checklist?</h4>
-        <p>
-          We're looking to field ideas from passionate advocates and subject-matter experts like
-          you. The roadmap for the platform consists of input from the r/latestage community on
-          Reddit; the following template helps us nail down specifics for your idea and allows for
-          easy posting on Reddit for the community to see:
-        </p>
+  <div class="disclosure">
+    <Card>
+      <h4><IdeaIcon /> Have an idea for the checklist?</h4>
+      <p>
+        We're looking to field ideas from passionate advocates and subject-matter experts like you.
+        The roadmap for the platform consists of input from the r/latestage community on Reddit; the
+        following template helps us nail down specifics for your idea and allows for easy posting on
+        Reddit for the community to see:
+      </p>
 
-        <a href="/checklist-rfcs#checklist-idea-template">Checklist Idea Template</a>
-      </Card>
-    </div>
+      <a href="/checklist-rfcs#checklist-idea-template">Checklist Idea Template</a>
+    </Card>
   </div>
 </main>
 
@@ -53,6 +57,10 @@
 
   .disclosure {
     margin-top: 16px;
+  }
+
+  :global(.checklist .checklist-item:last-of-type .card) {
+    margin-bottom: 64px !important;
   }
 
   .disclosure *:not(a) {
