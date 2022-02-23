99dd53a174f28cbcb4402172fa8ba5e07d23fb5a
jeffDevelops <jefferydreynolds@gmail.com>
2022-02-22 22:48:28 -0700

    PR review fixes

--- apps/api/src/resolvers/query/index.ts
+++ apps/api/src/resolvers/query/index.ts
@@ -1,4 +1,3 @@
 export { CurrentUserResolver } from './CurrentUserResolver'
 export { ImageKitAuthenticationParamsResolver } from './ImageKitAuthParamsResolver'
-export { UserBankExodusCompletionsCountResolver } from './UserBankExodusCompletionsCountResolver'
 export { RecaptchaVerificationResolver } from './RecaptchaVerificationResolver'
